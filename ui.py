b0f693b9ee0210a079abd6d39ade3c3d2f54b2c3
klauss1977 <klauss1977@yahoo.com>
2023-11-27 16:18:13 +0200

    final commit

--- ui.py
+++ ui.py
@@ -25,15 +25,39 @@ class QuizInterface:
         self.score_lbl = Label(text="Score: 0", fg='white', bg=THEME_COLOR)
         self.score_lbl.grid(row=0, column=1)
         true_img = PhotoImage(file="images/true.png")
-        self.true_btn = Button(image=true_img, highlightthickness=0)
+        self.true_btn = Button(image=true_img, highlightthickness=0, command=self.get_answer_true)
         self.true_btn.grid(row=2, column=0)
         false_img = PhotoImage(file="images/false.png")
-        self.false_btn = Button(image=false_img, highlightthickness=0)
+        self.false_btn = Button(image=false_img, highlightthickness=0, command=self.get_answer_false)
         self.false_btn.grid(row=2, column=1)
         self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.score_lbl.config(text=f"Score: {self.quiz.score}")
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz")
+            self.true_btn.config(state="disabled")
+            self.false_btn.config(state="disabled")
+
+    def get_answer_true(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+        #self.get_next_question()
+
+
+    def get_answer_false(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+        #self.get_next_question()
+
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
